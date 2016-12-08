44277ba60d8a29c5ffabb15c80e5b29c9bab6b1f
NicoZarate <nico.zarate1984@gmail.com>
2016-12-08 20:44:46 -0300

    probando las coockies y arreglando lo crud de simpletask

--- entrega/app/controllers/lists_controller.rb
+++ entrega/app/controllers/lists_controller.rb
@@ -1,23 +1,28 @@
 class ListsController < ApplicationController
-	def index
-      @lists = List.all
+	before_action :buscarEnLista
+  
+  def index
+       @lists = List.all
+       listCookie
+       @lists=to_array(cookies[:listCookie])
     end
 
 
 	def show
 
-    @list = List.find_by(url: params[:id])
+   # @list = List.find_by(url: params[:id])
   end
 
 
 	def new
 		@list=List.new
+    add_list @list
 	end
 	def edit
-       @list = List.find_by(url: params[:id])
+     #  @list = List.find_by(url: params[:id])
     end
     def update
-       @list = List.find_by(url: params[:id])
+      # @list = List.find_by(url: params[:id])
  
      if @list.update(list_params)
           redirect_to @list
@@ -28,19 +33,40 @@ end
 	def create
 		@list = List.new(list_params)
         if @list.save
+           add_list @list
            redirect_to @list
        else
        	  render  'new'
        end
     end
     def destroy
-       @list = List.find_by(url: params[:id])
+      # @list = List.find_by(url: params[:id])
        @list.destroy
  
       redirect_to lists_path
     end
     private
+    def buscarEnLista
+
+      @list = List.find_by(url:params[:id])
+      
+    end
     def list_params
        params.require(:list).permit(:name, :url)
     end
+    def listCookie
+       cookies[:listCookies] ||= ''
+   end
+
+   def to_array(st)
+     st.split(',')
+  end
+   def to_st(arreglo)
+     arreglo.join(',')
+   end
+   def add_list(lista)
+      @aux= to_array(cookies[:listCookie])
+      @aux<<lista
+      cookies[:listCookie]= to_st @aux
+   end
 end
--- entrega/app/controllers/simple_tasks_controller.rb
+++ entrega/app/controllers/simple_tasks_controller.rb
@@ -1,20 +1,61 @@
 class SimpleTasksController < ApplicationController
-	
+	before_action :buscarEnLista
+  before_action :buscarTask , only: [:update,:destroy, :edit]
+
 	def new
 		@simpleTask=SimpleTask.new
 	end	
 
 	def create
-       @list = List.find_by(params[:list_url])
-       @list_id = List.find_by(params[:list_url]).id
+     #  @list = List.find_by(params[:list_url])
+      # @list_id = List.find_by(params[:list_url]).id
        @simpleTask=SimpleTask.new(task_params)
-       @simpleTask.list_id=@list_id
-       @simpleTask.save()
-       redirect_to list_path(@list)
+       @simpleTask.list_id=@list.id
+       if @simpleTask.save
+          redirect_to @list
+        else
+           render 'new'
+         end 
+            
     end
+    def edit
+       
+    end
+
+    def update
+      # @simpleTask = SimpleTask.find_by(params[:id])
+ 
+     if @simpleTask.update(task_params)
+          redirect_to @list
+      else
+       render 'edit'
+     end
+  end
+
+  def destroy
+      # @list = List.find_by(url:params[:id])
+       #@simpleTask=@list.tasks.find_by(params[:id])
+       
+       @simpleTask.destroy
+ 
+      redirect_to lists_path(@list)
+    end
+
+
 	
 	private
+    def buscarEnLista
+
+      @list = List.find_by(url: params[:list_id])
+      
+    end
+    def buscarTask
+       @simpleTask=SimpleTask.find(params[:id])
+    end
     def task_params
       params.require(:simple_task).permit(:description, :state ,:priority)
     end
+    def listCookie
+      cookies[:cookies] ||= '' 
+   end
 end
